381fed95c004d87d91425e7dc27e0461d07bbd6a
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-21 10:35:49 +0530

    songlist component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { selectSong } from '../actions';  // not necessary to write file name because the filename is index.js
+import SongList from './SongList';
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <SongList />
+        </div>
     );
 }
 
