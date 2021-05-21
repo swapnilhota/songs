f48b67c4df2e5051f88238c72ac5dad06e8185b2
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-21 10:53:00 +0530

    display songList

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,11 +2,36 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends React.Component {
+
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary">
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">
+                        {song.title}
+                    </div>
+                </div>
+            )
+        })
+    }
+
     render() {
         return (
-            <div>SongList</div>
+            <div className="ui divided list">
+                {this.renderList()}
+            </div>
         );
     };
 }
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+    //by convention this naming
+    return { songs: state.songs };
+}
+
+export default connect(mapStateToProps)(SongList);   // connect() returns a function, second set of parenthesis invokes that function 
