63d09f8eccc87aab0ccc1a4a6db94b988af0d610
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-21 11:15:24 +0530

    showing selected song detail

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = () => {
+const SongDetail = ({ song }) => {
+
+    if (!song) {
+        return <div>Select a Song</div>
+    }
+
     return (
         <div>
-            Song Detail
+            <h3>Deatils for:</h3>
+            <p>
+                Title: {song.title}
+                <br />
+                Duration: {song.duration}
+            </p>
         </div>
     );
 }
+
+const mapStateToProps = (state) => {
+    return { song: state.selectedSong }
+}
+
+export default connect(mapStateToProps)(SongDetail);
